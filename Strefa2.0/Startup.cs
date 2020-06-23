6fbdad68a19bcd53fdaa31b237dce3955d2c26a5
Kapejos12 <kacper.makowski1@gmail.com>
2020-06-24 00:33:38 +0200

    Create project template

---
+++ Strefa2.0/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Strefa2._0.Startup))]
+namespace Strefa2._0
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
