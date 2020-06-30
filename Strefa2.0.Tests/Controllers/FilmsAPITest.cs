f9c843a140b12441a0c2fe613092949a25f411d4
Kapejos12 <kacper.makowski1@gmail.com>
2020-06-30 20:50:31 +0200

    bug fixing #1 bad tests library

---
+++ Strefa2.0.Tests/Controllers/FilmsAPITest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Strefa2._0.Controllers;
+using Xunit;
+using System.Net.Http;
+using Assert = Xunit.Assert;
+
+namespace Strefa2._0.Tests.Controllers
+{
+    public class FilmsAPITest
+    {
+        string execute(string query)
+        {
+            string url = "http://www.omdbapi.com/?apikey=5cb2a2c4&s=" + query;
+
+            return new HttpClient().GetAsync(url).Result.Content.ReadAsStringAsync().Result;
+        }
+
+        [Fact]
+        public void VideoAPIResponse()
+        {
+            string result = execute("Iron Man 3");
+
+            Assert.NotNull(result);
+        }
+
+    }
+}
---
+++ Strefa2.0/Controllers/VideoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+
+namespace Strefa2._0.Controllers
+{
+    public class VideoController : Controller
+    {
+
+        string execute(string query)
+        {
+            string url = "http://www.omdbapi.com/?apikey=5cb2a2c4&s=" + query;
+
+            return new HttpClient().GetAsync(url).Result.Content.ReadAsStringAsync().Result;
+        }
+
+        // GET: Video
+        public ActionResult Index()
+        {
+            
+
+            
+            
+            return View();
+        }
+    }
+}
